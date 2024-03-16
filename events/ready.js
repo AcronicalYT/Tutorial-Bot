ff8116102a8d77e32d04a33f4861815a5273e6e4
AcronicalYT <ace36501@hotmail.com>
2024-03-16 21:56:40 +0000

    Monitoring Site

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,20 @@
 const client = require('../index.js');
 const { Events, ActivityType } = require('discord.js');
+const express = require('express');
+const app = express();
+const port = 22567;
 
 client.once(Events.ClientReady, clientReady => {
     // clientReady.user.setActivity(`over ${clientReady.guilds.cache.size} servers`, { type: ActivityType.Watching });
     // clientReady.user.setStatus('dnd');
+
+    app.get('/', (req, res) => {
+        res.status(200).send('Tutorial Bot is online!');
+    });
+
+    app.listen(port, () => {
+        console.log(`Tutorial bot is listening at http://localhost:${port}`);
+    });
 
     const activities = [
         `over ${clientReady.guilds.cache.size} servers`,
